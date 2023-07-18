0f114e80e3427b2808476532270c7a8eeb52c0d1
Putri Defa <putri@aqi.co.id>
2023-07-18 22:14:21 +0700

    fixing update artikel

--- pages/article/proses_edit.php
+++ pages/article/proses_edit.php
@@ -17,31 +17,27 @@ $caption = $_POST['caption'];
 $updated_by_name = $_POST['updated_by_name'];
 $slug = slug($title);
 
-if ($title 	== "") 
-{
-	echo "Harap Isi Data Dengan Lengkap";
-}
-else {
+
+if (empty($gambar)) {
+	$query = mysqli_query($koneksi,"UPDATE `article` SET  
+	        title='$title',
+			short_description='$short_description',
+			description='$description',
+			caption='$caption',
+			slug='$slug ',
+			updated_by_name='$updated_by_name' WHERE id='$id';") or die(mysqli_error($koneksi));
+}else {
 	$gambar = move_uploaded_file($fileTmpName, $target.$newFileName);
-	if ($gambar) {
-		$query = mysqli_query ($koneksi,"UPDATE article SET     	
-		image = '$newFileName',
+	$query = mysqli_query($koneksi,"UPDATE `article` SET  
+			image = '$newFileName',
 			title='$title',
 			short_description='$short_description',
 			description='$description',
 			caption='$caption',
 			slug='$slug ',
-			updated_by_name='$updated_by_name' WHERE id='$id';")
-	 or die(mysqli_error($koneksi));
-	}
-    header('location:../../admin/index.php?page=article&aksi');
-?>
-<div class="alert alert-primary alert-dismissible fade show" role="alert">
-    Selamat Data Berhasil Disimpan
-  	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-   </button>
-</div>
-<?php 
+			updated_by_name='$updated_by_name' WHERE id='$id';") or die(mysqli_error($koneksi));
 }
- ?>
+
+header('location:../../admin/index.php?page=article&aksi');
+
+?>
